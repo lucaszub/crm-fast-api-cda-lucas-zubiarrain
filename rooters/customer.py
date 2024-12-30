38937b04d80a4cb5d037040af6d3c45e3a03ba81
Lucas <zubiarrainlucas@gmail.com>
2024-12-30 12:23:07 +0100

    fix root

--- rooters/customer.py
+++ rooters/customer.py
@@ -7,19 +7,19 @@ from services.customer_service import CustomerService
 router = APIRouter()
 
 # Route pour créer un client
-@router.post("/customers/", response_model=Customer)
+@router.post("/", response_model=Customer)
 def create_customer(customer: CustomerCreate, db: Session = Depends(get_db)):
     created_customer = CustomerService.create_customer(db=db, customer=customer)
     return Customer.from_orm(created_customer)
 
 # Route pour lister tous les clients
-@router.get("/customers/list", response_model=list[CustomerOut])
+@router.get("/list", response_model=list[CustomerOut])
 def list_customers(db: Session = Depends(get_db)):
     customers = CustomerService.get_customers(db=db)
     return [CustomerOut.from_orm(customer) for customer in customers]
 
 # Route pour récupérer un client par ID
-@router.get("/customers/{customer_id}", response_model=CustomerOut)
+@router.get("/{customer_id}", response_model=CustomerOut)
 def get_customer_by_id(customer_id: int, db: Session = Depends(get_db)):
     customer = CustomerService.get_customer_by_id(db=db, customer_id=customer_id)
     if customer is None:
@@ -27,7 +27,7 @@ def get_customer_by_id(customer_id: int, db: Session = Depends(get_db)):
     return CustomerOut.from_orm(customer)
 
 # Route pour mettre à jour un client
-@router.put("/customers/{customer_id}", response_model=CustomerOut)
+@router.put("/{customer_id}", response_model=CustomerOut)
 def update_customer(customer_id: int, customer: CustomerCreate, db: Session = Depends(get_db)):
     updated_customer = CustomerService.update_customer(db=db, customer_id=customer_id, customer=customer)
     if updated_customer is None:
@@ -35,7 +35,7 @@ def update_customer(customer_id: int, customer: CustomerCreate, db: Session = De
     return CustomerOut.from_orm(updated_customer)
 
 # Route pour supprimer un client
-@router.delete("/customers/{customer_id}", response_model=CustomerOut)
+@router.delete("/{customer_id}", response_model=CustomerOut)
 def delete_customer(customer_id: int, db: Session = Depends(get_db)):
     deleted_customer = CustomerService.delete_customer(db=db, customer_id=customer_id)
     if deleted_customer is None:
